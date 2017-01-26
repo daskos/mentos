e3821da3efeeded6253449f1095b11681a5eefdd
arttii <a.topchyan@reply.de>
2017-01-26 14:10:48 +0100

    ConnectionRefusedError 2.7 3

--- mentos/exceptions.py
+++ mentos/exceptions.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import socket
 
 try:
- 	ConnectionRefusedError
+    ConnectionRefusedError = ConnectionRefusedError
 except NameError:
 	ConnectionRefusedError = socket.error
 
