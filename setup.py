1e80b9bc376fcfa94ae107ce7192d667851ed6fe
arttii <a.topchyan@reply.de>
2017-01-30 16:46:11 +0100

    added release

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(name='mentos',
                         else ''),
       classifiers=[
             "Intended Audience :: Developers",
+            'License :: OSI Approved :: Apache Software License',
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
       ],
